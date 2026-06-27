e5e8a40876e91e8bcf7f13e8ae1fe3976e0da0bc
MisterRileyD <misternaughtyperson@gmail.com>
2026-06-27 15:54:51 -0400

    Add files via upload

---
+++ discord_config.js
@@ -0,0 +1,2 @@
+// discord_config.js
+window.FALLEN_GATE_DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1382809889684062298/We1eJedsfCJq11wfDdUU9zmzrQC5JbiUs6JqYgCQHmzBzguMMJE0TPyMCgCQ4VfO93Rt";
